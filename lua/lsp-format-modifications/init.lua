1b307ec983f5e312d2ad9ff22b5e8084afea848f
Joe Ellis <joechrisellis@gmail.com>
2026-05-09 10:46:06 +0100

    Use client.supports_method(…) instead of reading client.server_capabilities
    From https://neovim.io/doc/user/news-0.10/#news-0.10:
    
        Dynamic registration of LSP capabilities. An implication of this
        change is that checking a client's server_capabilities is no longer
        a sufficient indicator to see if a server supports a feature.
        Instead use client.supports_method(<method>). It considers both the
        dynamic capabilities and static server_capabilities.
    
    This also allows us to pass in the bufnr (which I presume is because, as
    the docs note, some capabilities can be file-specific).

--- lua/lsp-format-modifications/init.lua
+++ lua/lsp-format-modifications/init.lua
@@ -41,7 +41,7 @@ local base_config = {
 }
 
 local function prechecks(lsp_client, bufnr, config)
-  if not lsp_client.server_capabilities.documentRangeFormattingProvider then -- unsupported server
+  if not lsp_client.supports_method("textDocument/rangeFormatting", bufnr) then -- unsupported server
     return "client " .. lsp_client.name .. " does not have a document range formatting provider"
   end
 
@@ -234,8 +234,7 @@ M.format_modifications = function(lsp_client, bufnr, config)
 
   local comparee_content = table.concat(comparee_lines, "\n")
 
-  local rangeFormattingProvider = lsp_client.server_capabilities.documentRangeFormattingProvider
-  if type(rangeFormattingProvider) == "table" and rangeFormattingProvider.rangesSupport then
+  if lsp_client.supports_method("textDocument/rangesFormatting", bufnr) then
     format_modifications_bulk(lsp_client, bufnr, config, comparee_content)
   else
     format_modifications_iterative(lsp_client, bufnr, config, comparee_content)
